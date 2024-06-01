0e6e00cc5f704a4f0cba5699d5ddb08858e9cb33
HJTN <zintnm21@gmail.com>
2024-06-02 04:03:49 +0900

    M: Login Token Message

--- app/src/main/java/com/example/ridingbud/ui/user/LoginActivity.kt
+++ app/src/main/java/com/example/ridingbud/ui/user/LoginActivity.kt
@@ -69,7 +69,7 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "로그인 정보 확인 - 토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
             }
             else if (tokenInfo != null) {
-//                Toast.makeText(this, "로그인 정보 확인 - 토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인 정보 확인 - 토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                 finish()
